f6d650054649c9e72bbaa2a907e11c7d18026760
Sandra Luo <hello@sandraluo.com>
2018-01-11 22:47:05 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Submission for CodePath iOS class at UC San Diego
+Sandra Luo
+hello@sandraluo.com
+January 11, 2018
--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calcTip(_ sender: AnyObject) {
+        let tipPercentages = [0.15,0.18,0.2]
+        
+        //"let" denotes unchanging variable value
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //%f for inserting decimal numbers
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
